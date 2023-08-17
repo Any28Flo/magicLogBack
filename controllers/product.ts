b17cc75db415634fc4a4b2f60d3159f1e5c8ed06
Analin Flores <foglzerika@gmail.com>
2023-08-17 08:45:26 -0600

    feat | Create routes to getProducts getProduc and filter by seller

--- controllers/product.ts
+++ controllers/product.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express";
 import { BaseProduct } from "../interfaces/products.interface";
 import ProductModel from "../models/Products";
+import UserModel from "../models/User";
 
 const postProduct = async (req: Request, res: Response) => {
-    console.log(req.user)
-    console.log(req.uid)
 
     const product: BaseProduct = req.body;
     try {
@@ -23,7 +22,86 @@ const postProduct = async (req: Request, res: Response) => {
         })
     }
 }
+const getProductsByUser = async (req: Request, res: Response) => {
+    const user = req.user;
+    const id = user?._id;
 
+    try {
+
+        const products = await ProductModel.find({ registerBy: id });
+
+        res.status(201).json({
+            msg: 'lista productos',
+            products
+        })
+    } catch (error) {
+
+        throw new Error('Comuniquese con soporte.');
+    }
+}
+const getProducts = async (req: Request, res: Response) => {
+
+
+    try {
+
+        const products = await ProductModel.find();
+
+        res.status(201).json({
+            msg: 'lista productos',
+            products
+        })
+    } catch (error) {
+
+        throw new Error('Comuniquese con soporte.');
+    }
+}
+const getCustomProducts = async (req: Request, res: Response) => {
+    const { keyword, sku, sort, orderBy } = req.query;
+
+    const filters: any = {};
+    const sortOptions: any = {};
+
+
+    if (!keyword || typeof keyword !== 'string') {
+        return res.status(400).json({ error: 'Parametro invalido, falta filtro keyword' });
+    }
+
+    if (keyword) {
+        const keywordPattern = new RegExp(keyword, 'i');
+        filters.name = keywordPattern;
+    }
+
+    if (sku) {
+        filters.sku = sku;
+    }
+
+    if (orderBy) {
+        sortOptions[orderBy as string] = 1; // Ascending order
+    }
+    if (sort === 'asc') {
+        sortOptions.price = 1; // Ascending order
+    } else if (sort === 'desc') {
+        sortOptions.price = -1; // Descending order
+    }
+
+
+
+    try {
+
+        const products = await ProductModel.find(filters).sort(sortOptions);
+
+        res.status(201).json({
+            msg: 'lista productos',
+            products
+        })
+    } catch (error) {
+
+        throw new Error('Comuniquese con soporte.');
+    }
+}
 export {
-    postProduct
+    getProductsByUser,
+    getProducts,
+    postProduct,
+    getCustomProducts
 }
--- routes/products.routes.ts
+++ routes/products.routes.ts
@@ -4,20 +4,44 @@ import { body } from "express-validator";
 import validateJWT from "../middlewares/validate_jwt";
 import validateFields from "../middlewares/validate_fields";
 
-import { postProduct } from "../controllers/product";
 
 import { isProductExist } from "../helpers/db-validators";
+import { isBuyerRole, isSellerRole } from "../middlewares/validate_rol";
 
+import {
+    postProduct,
+    getProductsByUser,
+    getProducts,
+    getCustomProducts
+} from "../controllers/product";
 
 const productRoutes = Router();
 
 productRoutes.post('/', [
     validateJWT,
+    isSellerRole,
     body('sku').notEmpty().withMessage('El sku es requerido').custom(isProductExist),
     body('name').notEmpty().withMessage('El nombre es requerido'),
     body('amount').notEmpty().withMessage('La cantidad es requerido'),
     body('price').notEmpty().withMessage('El precio es requerido'),
     validateFields
 ], postProduct)
+
+productRoutes.get('/', [
+    validateJWT,
+    isSellerRole
+], getProductsByUser)
+
+
+productRoutes.get('/lista-productos', [
+    validateJWT,
+    isBuyerRole,
+], getProducts)
+
+productRoutes.get('/lista-custom-products', [
+    validateJWT,
+    isBuyerRole,
+], getCustomProducts)
+
 
 export default productRoutes;
